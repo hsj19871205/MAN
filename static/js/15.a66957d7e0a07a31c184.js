bab488b59c6a821a9790988984ea17e4601352d5
hsj19871205 <391509677@qq.com>
2021-08-06 11:06:13 +0800

    test_commit

---
+++ static/js/15.a66957d7e0a07a31c184.js
@@ -0,0 +1,2 @@
+webpackJsonp([15],{v86N:function(e,r,t){"use strict";Object.defineProperty(r,"__esModule",{value:!0});var u=t("VU/8")({created:function(){var e=this.$route,r=e.params,t=e.query,u=r.path;this.$router.replace({path:"/"+u,query:t})},render:function(e){return e()}},null,!1,null,null,null);r.default=u.exports}});
+//# sourceMappingURL=15.a66957d7e0a07a31c184.js.map
